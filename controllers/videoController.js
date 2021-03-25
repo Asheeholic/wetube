7307f3d5da8fc14a27f742f1681ed137571d3ed9
한재호 <hanjaeho@hanjaehoui-iMac.local>
2021-03-25 12:22:45 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,15 +1,24 @@
-export const home = (req, res) => res.render("home",
- { pageTitle: "Home" });
-export const search = (req, res) => res.render("search",
- { pageTitle: "Search" });
+export const home = (req, res) =>
+res.render("home", { pageTitle: "Home" });
 
-export const videos = (req, res) => res.render("videos",
- { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("upload",
- { pageTitle: "Upload" });
-export const videoDetail = (req, res) => res.render("videoDetail",
- { pageTitle: "Video Detail" });
-export const editVideo = (req, res) => res.render("editVideo",
- { pageTitle: "Edit Video" });
-export const deleteVideo = (req, res) => res.render("deleteVideo",
- { pageTitle: "Delete Video" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }  // better way than req.query.term
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) =>
+ res.render("videos", { pageTitle: "Videos" });
+
+export const upload = (req, res) =>
+res.render("upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) => 
+res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+res.render("deleteVideo", { pageTitle: "Delete Video" });
