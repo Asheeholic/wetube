1a4966240bb37dfc392e81d0e0a42f0660e1bc0b
Han Jae Ho <gkdtjdrb95@gmail.com>
2021-03-25 14:28:06 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 123123,
+    title: "video good",
+    description: "This is good video",
+    views: 24,
+    videoFile:"https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Han Jae Ho",
+      email: "gkstjdrb95@naver.com"
+    }
+  },
+  {
+    id: 112323,
+    title: "video super",
+    description: "This is good video",
+    views: 24,
+    videoFile:"https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Han Jae Ho",
+      email: "gkstjdrb95@naver.com"
+    }
+  },
+  {
+    id: 123242,
+    title: "video Dame Good",
+    description: "This is good video",
+    views: 24,
+    videoFile:"https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Han Jae Ho",
+      email: "gkstjdrb95@naver.com"
+    }
+  },
+];
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { upload, videoDetail, videos, editVideo, deleteVideo } from "../controllers/videoController";
+import { upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
