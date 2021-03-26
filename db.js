7380c90a435c4b221417c573ad114e66206a73b9
Han Jae Ho <gkdtjdrb95@gmail.com>
2021-03-26 21:43:39 +0900

    Connecting to mongoDB

--- db.js
+++ db.js
@@ -1,38 +1,17 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb://localhost:27017/we-tube", 
   {
-    id: 123123,
-    title: "video good",
-    description: "This is good video",
-    views: 24,
-    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Han Jae Ho",
-      email: "gkstjdrb95@naver.com"
-    }
-  },
-  {
-    id: 112323,
-    title: "video super",
-    description: "This is good video",
-    views: 24,
-    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Han Jae Ho",
-      email: "gkstjdrb95@naver.com"
-    }
-  },
-  {
-    id: 123242,
-    title: "video Dame Good",
-    description: "This is good video",
-    views: 24,
-    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Han Jae Ho",
-      email: "gkstjdrb95@naver.com"
-    }
-  },
-];
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = () => console.log(`❌  Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
